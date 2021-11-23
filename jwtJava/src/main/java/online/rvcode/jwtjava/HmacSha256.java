52547a06e8ea998beeb8d995bd6a477331ee33af
Rubens Lyra <rubensvlyra@gmail.com>
2021-11-23 10:21:55 -0300

    corrigindo hmacsha256

--- jwtJava/src/main/java/online/rvcode/jwtjava/HmacSha256.java
+++ jwtJava/src/main/java/online/rvcode/jwtjava/HmacSha256.java
@@ -6,13 +6,17 @@ import java.security.NoSuchAlgorithmException;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
+
 public class HmacSha256 {
+    
 	public static String sign(String secret, String data){
 		try{
 		
 		Mac sha256Hmac = Mac.getInstance("HmacSHA256");
 		
 		SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256");
+                sha256Hmac.init(secretKey);
+                
 		byte[] signedBytes = sha256Hmac.doFinal(data.getBytes("UTF-8"));
 		
 		return Base64Url.encode(signedBytes);
--- jwtJava/src/main/java/online/rvcode/jwtjava/JWTGenerator.java
+++ jwtJava/src/main/java/online/rvcode/jwtjava/JWTGenerator.java
@@ -4,6 +4,17 @@ package online.rvcode.jwtjava;
 public class JWTGenerator {
     public static void main(String[] args) {
         String header = "\"alg\": \"HS256\"";
+        
         String payload = "{ \"rvcode:nome\": \"Zezim\", \"rvcode:admin\": true }";
+        
+        String encodeData = Base64Url.encode(header) + "." + Base64Url.encode(payload);
+        
+        String secret = "0Segr3do";
+        
+        String signature = HmacSha256.sign(secret, encodeData);
+        
+        String jwt = encodeData + "." + signature;
+        
+        System.out.println(encodeData);
     }
 }
